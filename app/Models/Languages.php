fddb317156a670c487c6360f786fe746ecbbbeaf
Poongodigit <135576454+Poongodigit@users.noreply.github.com>
2023-09-27 15:29:07 +0530

    Add files via upload

---
+++ app/Models/Languages.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Languages extends Model
+{
+      /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+
+     protected $table = 'language_master';
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+   
+}
